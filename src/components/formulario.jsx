2066ea6897ac013959de33b658ada27dc12ebaf1
Daniel151505 <daniel150520011@gmail.com>
2022-12-03 23:00:41 -0500

    Eventos y Sintaxis en React

--- src/components/formulario.jsx
+++ src/components/formulario.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react"
 
 const Formulario = () => {
-  cosnt [nombre, setNombre] = useState('Hook');
+  const [nombre, setNombre] = useState('Hook');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Enviando Formulario')
+  }
 
   return (
     <div className="md:w-1/2 lg:w-2/5">
@@ -12,7 +17,9 @@ const Formulario = () => {
           <span className="text-indigo-600 font-bold">Administralos</span>
         </p>
         
-        <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        <form 
+          onSubmit={handleSubmit}
+          className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
           <div className="mb-5">
             <label 
               htmlFor="mascota"
