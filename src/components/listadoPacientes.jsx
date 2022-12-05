fa67f1a7b54139c1f204f03e6cf69a99e1f8f7ba
Daniel151505 <daniel150520011@gmail.com>
2022-12-04 20:39:02 -0500

    Generar Id para cada paciente

--- src/components/listadoPacientes.jsx
+++ src/components/listadoPacientes.jsx
@@ -1,6 +1,6 @@
 import Paciente from "./Paciente";
 
-function ListadoPacientes({ paciente }) {
+function ListadoPacientes({ pacientes }) {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
       <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
@@ -9,9 +9,9 @@ function ListadoPacientes({ paciente }) {
         <span className="text-indigo-600 font-bold">Pacientes y citas</span>
       </p>
 
-      {pacientes.map((paciente, index) => (
+      {pacientes.map( paciente => (
         <Paciente 
-            key={index} 
+            key={paciente.id}
             paciente={paciente} 
         />
       ))}
