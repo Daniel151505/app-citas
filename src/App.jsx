3b4aa5f40b5527d55988f7c619cedaabcb1b7615
Daniel151505 <daniel150520011@gmail.com>
2022-12-05 22:03:16 -0500

    Eliminando paciente

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ function App() {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState([]);
 
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id)
+  }
+
   return (
     <div className="container mx-auto mt-20">
       <Header />
@@ -20,6 +24,7 @@ function App() {
         <ListadoPacientes 
           pacientes={pacientes} 
           setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
         />
       </div>
     </div>
